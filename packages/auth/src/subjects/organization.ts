478b3cab4e3e6afe0ab42fa909127a4ba06658ca
wjuniori <wjuniori.si@gmail.com>
2026-01-16 18:03:10 -0300

    add module Setup do projeto e permissões

---
+++ packages/auth/src/subjects/organization.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+import { organizationSchema } from '../models/organization'
+
+export const organizationSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('update'),
+    z.literal('delete'),
+    z.literal('transfer_ownership'),
+  ]),
+  z.union([z.literal('Organization'), organizationSchema]),
+])
