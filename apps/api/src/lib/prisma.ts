92c4027faca42de68cd3972547be4628904bf339
wjuniori <wjuniori.si@gmail.com>
2026-01-17 00:27:21 -0300

    add module Desenvolvendo o back-end

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -1,5 +1,10 @@
-import { PrismaClient } from "@/generated/prisma/client";
+import 'dotenv/config'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@/generated/prisma/client'
 
-export const prisma = new PrismaClient({
-  log: ['query'],
-})
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter, log: ['query'] })
+
+export { prisma }
