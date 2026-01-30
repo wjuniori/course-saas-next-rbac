223d7af01fb50cae6a82eaaa79bdd9f25a8111ed
wjuniori <wjuniori.si@gmail.com>
2026-01-30 16:26:12 -0300

    add [module Desenvolvendo o front-end] lesson Action: Criação de projeto

--- apps/web/src/app/(app)/org/[slug]/create-project/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/create-project/actions.ts
@@ -3,7 +3,8 @@
 import { HTTPError } from 'ky'
 import { z } from 'zod'
 
-// import { createProject } from '@/http/create-project'
+import { getCurrentOrg } from '@/auth/auth'
+import { createProject } from '@/http/create-project'
 
 const projectSchema = z.object({
   name: z
@@ -22,13 +23,14 @@ export async function createProjectAction(data: FormData) {
   }
 
   const { name, description } = result.data
+  const org = await getCurrentOrg()
 
   try {
-    // await createProject({
-    //   name,
-    //   domain,
-    //   shouldAttachUsersByDomain,
-    // })
+    await createProject({
+      org: org!,
+      name,
+      description,
+    })
   } catch (err) {
     if (err instanceof HTTPError) {
       const { message } = await err.response.json()
