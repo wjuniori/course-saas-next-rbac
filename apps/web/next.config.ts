3c22c7baee3cf357798b16f902a638249b648677
wjuniori <wjuniori.si@gmail.com>
2026-02-03 15:35:01 -0300

    add [module Desenvolvendo o front-end] lesson Listagem de membros

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      { hostname: 'github.com' },
+      { hostname: 'avatars.githubusercontent.com' },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
