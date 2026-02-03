6120b695fca51be536ebb459a4c726bb1a5e8932
wjuniori <wjuniori.si@gmail.com>
2026-02-03 16:16:01 -0300

    add [module Desenvolvendo o front-end] lesson Atualizar cargo do membro

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -1,9 +1,11 @@
 'use server'
 
+import { Role } from '@saas/auth'
 import { revalidateTag } from 'next/cache'
 
 import { getCurrentOrg } from '@/auth/auth'
 import { removeMember } from '@/http/remove-member'
+import { updateMember } from '@/http/update-member'
 
 export async function removeMemberAction(memberId: string) {
   const currentOrg = await getCurrentOrg()
@@ -11,6 +13,18 @@ export async function removeMemberAction(memberId: string) {
   await removeMember({
     org: currentOrg!,
     memberId,
+  })
+
+  revalidateTag(`${currentOrg}/members`, 'max')
+}
+
+export async function updateMemberAction(memberId: string, role: Role) {
+  const currentOrg = await getCurrentOrg()
+
+  await updateMember({
+    org: currentOrg!,
+    memberId,
+    role,
   })
 
   revalidateTag(`${currentOrg}/members`, 'max')
