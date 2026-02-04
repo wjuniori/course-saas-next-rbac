344308f07bb130b59f659f8c269e8b6e1eda3b0c
wjuniori <wjuniori.si@gmail.com>
2026-02-04 13:05:34 -0300

    add [module Desenvolvendo o front-end] lesson Action: aceitar convite

--- apps/web/src/app/api/auth/callback/route.ts
+++ apps/web/src/app/api/auth/callback/route.ts
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
+import { acceptInvite } from '@/http/accept-invite'
 import { signInWithGithub } from '@/http/sign-in-with-github'
 
 export async function GET(request: NextRequest) {
@@ -22,6 +23,15 @@ export async function GET(request: NextRequest) {
     path: '/',
     maxAge: 60 * 60 * 24 * 7, // 7days
   })
+
+  const inviteId = cookieStore.get('inviteId')?.value
+
+  if (inviteId) {
+    try {
+      await acceptInvite(inviteId)
+      cookieStore.delete('inviteId')
+    } catch {}
+  }
 
   const redirectUrl = request.nextUrl.clone()
 
