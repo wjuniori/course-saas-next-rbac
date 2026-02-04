e6d7e4e9d5c18ee297a79bb3524ccc4be42f3df7
wjuniori <wjuniori.si@gmail.com>
2026-02-04 20:10:35 -0300

    add [module Deploy] lesson Deploy do Back-end

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -125,6 +125,6 @@ app.register(getPendingInvites)
 // Billing Routes
 app.register(getOrganizationBilling)
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
 })
